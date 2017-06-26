8d54083c4b15a4e1f8ac2c510e7aede6e570c417
Felipe Gbur <felipegbur@gmail.com>
2017-06-26 19:04:23 -0300

    Create codicoes.py

---
+++ codicoes.py
@@ -0,0 +1,9 @@
+idade = int(input("Informe sua idadde: 18"))
+if(idade <= 0):
+    ("Sua idade não pode ser igual a zero")
+elif(idade>150):
+    print("A sua idade não pode ser 150 anos")
+elif(idade < 18):
+    print("você precisa ter mais de 18 anos")
+elif(idade>= 18):
+    print("Você é maior de idade")
