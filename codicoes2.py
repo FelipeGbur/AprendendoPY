95ea8673134251d5368147b30a8093af657f4419
Felipe Gbur <felipegbur@gmail.com>
2017-06-26 19:04:58 -0300

    Create codicoes2.py

---
+++ codicoes2.py
@@ -0,0 +1,20 @@
+val1= int(input("Digite um Número"))
+val2= int(input("Digite um segundo Número"))
+
+if(val1 == val2): #Operador de Igualdade
+    print("Os valores %d e %d são iguais" %(val1,val2))
+
+if(val1 != val2):#diferente de
+    print("Os valores %d e %d são diferentes" %(val1,val2))
+
+if(val1 < val2):#menor que
+    print("O número %d é menor que %d"%(val1,val2))
+
+    if (val1 > val2):  # maior que
+        print("O número %d é maior que %d" % (val1, val2))
+
+if(val1>=val2):#maior igual
+    print("O valor %d é maior ou igual ao valor %d" %(val1,val2))
+
+    if (val1 <= val2):#menor igual
+        print("O valor %d é menor ou igual ao valor %d" % (val1, val2))
