be8fe2cf102a70463e3cdb7f810240215852c867
Felipe Gbur <felipegbur@gmail.com>
2017-07-10 11:06:56 -0300

    Create ex006.py

---
+++ ex006.py
@@ -0,0 +1,7 @@
+n1 = int((input('Digite um número:')))
+x2 = n1*2
+x3 = n1*3
+ra = n1**(1/2)
+print('O dobro de {} é: {}'.format(n1, x2))
+print('O triplo de {} é {}'.format(n1, x3))
+print('A raiz quadrada de {} é: {:.2f}'.format(n1, ra))
