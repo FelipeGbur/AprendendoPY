5f4e064a716bf6815e0ce995c4c35b972cd0af4b
Felipe Gbur <felipegbur@gmail.com>
2017-07-10 11:07:52 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,5 @@
+nota1 = float(input('Dgite a primeira nota do aluno: '))
+nota2 = float(input('Dgite a segunda nota do aluno: '))
+s = nota1 + nota2
+med = s/2
+print('A média entre a primeira nota {} e a segunda nota {} é:{} '.format(nota1, nota2, med))
